50eb7ac0a29a23b25a483d2cb9cd378f03de528a
Christian De la Hoz <christian.hoz@anixe.pl>
2021-09-27 17:30:50 +0200

    clean up - split compiler / parser

--- src/compiler.zig
+++ src/compiler.zig
@@ -1,48 +1,34 @@
 const std = @import("std");
 const debug = std.debug;
-const Scanner = @import("scanner.zig").Scanner;
 const Vm = @import("vm.zig").Vm;
 const Token = @import("scanner.zig").Token;
 const Chunk = @import("chunk.zig").Chunk;
 const OpCode = @import("chunk.zig").OpCode;
 const Value = @import("value.zig").Value;
 const Obj = @import("object.zig").Obj;
+const Parser = @import("parser.zig").Parser;
 const d = @import("debug.zig");
 
 pub const Compiler = struct {
     const Self = @This();
     vm: *Vm,
-    scanner: Scanner,
     chunk: *Chunk,
-    current: Token = undefined,
-    previous: Token = undefined,
-    had_error: bool = false,
-    panic_mode: bool = false,
+    parser: Parser,
 
     pub fn init(vm: *Vm, source: []const u8, chunk: *Chunk) Self {
         return Self{
             .vm = vm,
-            .scanner = Scanner.init(source),
+            .parser = Parser.init(source),
             .chunk = chunk,
         };
     }
 
-    fn match(self: *Self, ty: Token.Type) bool {
-        if (!self.check(ty)) return false;
-        self.advance();
-        return true;
-    }
-
-    fn check(self: Self, ty: Token.Type) bool {
-        return self.current.ty == ty;
-    }
-
     pub fn compile(self: *Self) error{Compile}!void {
-        self.advance();
-        while (!self.match(Token.Type.eof)) {
+        self.parser.advance();
+        while (!self.parser.match(Token.Type.eof)) {
             self.declaration();
         }
-        if (self.had_error) {
+        if (self.parser.had_error) {
             return error.Compile;
         }
         return self.end();
@@ -50,35 +36,35 @@ pub const Compiler = struct {
 
     fn printStatement(self: *Self) void {
         self.expression();
-        self.consume(.semicolon, "Expect ';' after value.");
+        self.parser.consume(.semicolon, "Expect ';' after value.");
         self.emitOp(OpCode.print);
     }
 
     fn declaration(self: *Self) void {
-        if (self.match(.@"var")) {
+        if (self.parser.match(.@"var")) {
             self.varDeclaration();
         } else {
             self.statement();
         }
 
-        if (self.panic_mode) {
-            self.synchronize();
+        if (self.parser.panic_mode) {
+            self.parser.synchronize();
         }
     }
 
     fn varDeclaration(self: *Self) void {
         const global = self.parseVariable("Expect variable name.") catch unreachable;
-        if (self.match(.equal)) {
+        if (self.parser.match(.equal)) {
             self.expression();
         } else {
             self.emitOp(.nil);
         }
-        self.consume(.semicolon, "Expect ';' after variable declaration.");
+        self.parser.consume(.semicolon, "Expect ';' after variable declaration.");
         self.defineVariable(global);
     }
 
     fn statement(self: *Self) void {
-        if (self.match(Token.Type.print)) {
+        if (self.parser.match(Token.Type.print)) {
             self.printStatement();
         } else {
             self.expressionStatement();
@@ -87,64 +73,8 @@ pub const Compiler = struct {
 
     fn expressionStatement(self: *Self) void {
         self.expression();
-        self.consume(.semicolon, "Expect ';' after expression.");
+        self.parser.consume(.semicolon, "Expect ';' after expression.");
         self.emitOp(OpCode.pop);
-    }
-
-    fn advance(self: *Self) void {
-        self.previous = self.current;
-        while (true) {
-            const next = self.scanner.nextToken();
-            if (next.ty == .@"error") {
-                self.errorAtCurrent(next.lexeme);
-            } else {
-                self.current = next;
-                break;
-            }
-        }
-    }
-
-    fn consume(self: *Self, ty: Token.Type, message: []const u8) void {
-        if (self.current.ty != ty) {
-            self.errorAtCurrent(message);
-        } else {
-            self.advance();
-        }
-    }
-
-    pub fn errorAtCurrent(self: *Self, message: []const u8) void {
-        self.errorAt(self.current, message);
-    }
-
-    pub fn errorHere(self: *Self, message: []const u8) void {
-        self.errorAt(self.previous, message);
-    }
-
-    fn synchronize(self: *Self) void {
-        self.panic_mode = false;
-        while (self.current.ty != .eof) {
-            if (self.previous.ty == .semicolon) return;
-            switch (self.current.ty) {
-                .class, .fun, .@"var", .@"for", .@"if", .@"while", .print, .@"return" => break,
-                else => self.advance(),
-            }
-        }
-    }
-
-    fn errorAt(self: *Self, token: Token, message: []const u8) void {
-        if (self.panic_mode) {
-            return;
-        } else {
-            self.panic_mode = true;
-        }
-        debug.print("[line {d} Error", .{token.line});
-        switch (token.ty) {
-            .eof => debug.print(" at end", .{}),
-            .@"error" => {},
-            else => debug.print(" at '{s}'", .{token.lexeme}),
-        }
-        debug.print(": {s}\n", .{message});
-        self.had_error = true;
     }
 
     fn expression(self: *Self) void {
@@ -152,20 +82,20 @@ pub const Compiler = struct {
     }
 
     fn number(self: *Self) void {
-        const val = std.fmt.parseFloat(f64, self.previous.lexeme) catch unreachable;
+        const val = std.fmt.parseFloat(f64, self.parser.previous.lexeme) catch unreachable;
         self.emitConstant(Value{ .number = val });
     }
 
     fn grouping(self: *Self) void {
         self.expression();
-        self.consume(.right_paren, "Expect ')' after expression.");
+        self.parser.consume(.right_paren, "Expect ')' after expression.");
     }
 
     fn binary(self: *Self) void {
-        const ty = self.previous.ty;
+        const ty = self.parser.previous.ty;
         const prec = ty.infix_prec();
         if (prec == null) {
-            self.errorHere("Invalid infix operator.");
+            self.parser.errorHere("Invalid infix operator.");
             return;
         }
 
@@ -189,7 +119,7 @@ pub const Compiler = struct {
     }
 
     fn unary(self: *Self) void {
-        const ty = self.previous.ty;
+        const ty = self.parser.previous.ty;
         self.parsePrecedence(.unary);
 
         switch (ty) {
@@ -200,7 +130,7 @@ pub const Compiler = struct {
     }
 
     fn literal(self: *Self) void {
-        switch (self.previous.ty) {
+        switch (self.parser.previous.ty) {
             .@"false" => self.emitOp(OpCode.@"false"),
             .@"true" => self.emitOp(OpCode.@"true"),
             .nil => self.emitOp(OpCode.nil),
@@ -209,19 +139,19 @@ pub const Compiler = struct {
     }
 
     fn string(self: *Self) void {
-        const lexeme = self.previous.lexeme;
+        const lexeme = self.parser.previous.lexeme;
         const new_string = Obj.String.copy(self.vm, lexeme) catch unreachable;
         const obj = Value{ .obj = &new_string.obj };
         self.emitConstant(obj);
     }
 
     fn variable(self: *Self, can_assign: bool) void {
-        self.namedVariable(self.previous, can_assign);
+        self.namedVariable(self.parser.previous, can_assign);
     }
 
     fn namedVariable(self: *Self, token: Token, can_assign: bool) void {
         const global = self.identifierConstant(token) catch unreachable;
-        if (can_assign and self.match(.equal)) {
+        if (can_assign and self.parser.match(.equal)) {
             self.expression();
             self.emitBytes(&[2]usize{ @enumToInt(OpCode.set_global), global });
         } else {
@@ -231,37 +161,37 @@ pub const Compiler = struct {
 
     // prefix, infix, precedence
     fn parsePrecedence(self: *Self, prec: Precedence) void {
-        self.advance();
+        self.parser.advance();
         const can_assign = @enumToInt(prec) <= @enumToInt(Precedence.assignment);
-        switch (self.previous.ty) {
+        switch (self.parser.previous.ty) {
             .left_paren => self.grouping(),
             .minus, .bang => self.unary(),
             .number => self.number(),
             .string => self.string(),
             .identifier => self.variable(can_assign),
             .@"false", .@"true", .nil => self.literal(),
-            else => return self.errorHere("Expected expression."),
+            else => return self.parser.errorHere("Expected expression."),
         }
 
-        while (self.current.ty.infix_prec()) |next_prec| {
+        while (self.parser.current.ty.infix_prec()) |next_prec| {
             if (@enumToInt(prec) > @enumToInt(next_prec)) {
                 break;
             }
-            self.advance();
-            switch (self.previous.ty) {
+            self.parser.advance();
+            switch (self.parser.previous.ty) {
                 .plus, .minus, .slash, .star, .equal_equal, .greater, .greater_equal, .less, .less_equal => self.binary(),
-                else => return self.errorHere("Invalid infix operator."),
+                else => return self.parser.errorHere("Invalid infix operator."),
             }
         }
 
-        if (can_assign and self.match(.equal)) {
-            self.errorHere("Invalid assignment target.");
+        if (can_assign and self.parser.match(.equal)) {
+            self.parser.errorHere("Invalid assignment target.");
         }
     }
 
     fn parseVariable(self: *Self, message: []const u8) !usize {
-        self.consume(.identifier, message);
-        return self.identifierConstant(self.previous);
+        self.parser.consume(.identifier, message);
+        return self.identifierConstant(self.parser.previous);
     }
 
     fn defineVariable(self: *Self, global: usize) void {
@@ -275,7 +205,7 @@ pub const Compiler = struct {
 
     fn end(self: *Self) void {
         self.emitReturn();
-        if (!self.had_error) {
+        if (!self.parser.had_error) {
             d.disassembleChunk(self.chunk.*, "code");
         }
     }
@@ -291,18 +221,10 @@ pub const Compiler = struct {
 
     fn makeConstant(self: *Self, value: Value) !usize {
         const constant = self.chunk.addConstant(value) catch |err| {
-            self.errorHere("Too many constants in one chunk");
+            self.parser.errorHere("Too many constants in one chunk");
             return err;
         };
         return constant;
-    }
-
-    fn emitByte(self: *Self, byte: usize) void {
-        self.chunk.write(byte, self.previous.line);
-    }
-
-    fn emitOp(self: *Self, op: OpCode) void {
-        self.emitByte(@enumToInt(op));
     }
 
     fn emitOps(self: *Self, ops: []const OpCode) void {
@@ -311,9 +233,17 @@ pub const Compiler = struct {
         }
     }
 
+    fn emitOp(self: *Self, op: OpCode) void {
+        self.emitByte(@enumToInt(op));
+    }
+
+    fn emitByte(self: *Self, byte: usize) void {
+        self.chunk.write(byte, self.parser.previous.line);
+    }
+
     fn emitBytes(self: *Self, bytes: []usize) void {
         for (bytes) |byte| {
-            self.chunk.write(byte, self.previous.line);
+            self.emitByte(byte);
         }
     }
 };
---
+++ src/parser.zig
@@ -0,0 +1,85 @@
+const std = @import("std");
+const debug = std.debug;
+const Scanner = @import("scanner.zig").Scanner;
+const Token = @import("scanner.zig").Token;
+
+pub const Parser = struct {
+    const Self = @This();
+    scanner: Scanner,
+    current: Token = undefined,
+    previous: Token = undefined,
+    had_error: bool = false,
+    panic_mode: bool = false,
+
+    pub fn init(source: []const u8) Self {
+        return Self{
+            .scanner = Scanner.init(source),
+        };
+    }
+
+    pub fn advance(self: *Self) void {
+        self.previous = self.current;
+        while (true) {
+            const next = self.scanner.nextToken();
+            if (next.ty == .@"error") {
+                self.errorAtCurrent(next.lexeme);
+            } else {
+                self.current = next;
+                break;
+            }
+        }
+    }
+
+    pub fn synchronize(self: *Self) void {
+        self.panic_mode = false;
+        while (self.current.ty != .eof) {
+            if (self.previous.ty == .semicolon) return;
+            switch (self.current.ty) {
+                .class, .fun, .@"var", .@"for", .@"if", .@"while", .print, .@"return" => break,
+                else => self.advance(),
+            }
+        }
+    }
+
+    pub fn errorHere(self: *Self, message: []const u8) void {
+        self.errorAt(self.previous, message);
+    }
+
+    pub fn errorAt(self: *Self, token: Token, message: []const u8) void {
+        if (self.panic_mode) {
+            return;
+        } else {
+            self.panic_mode = true;
+        }
+        debug.print("[line {d} Error", .{token.line});
+        switch (token.ty) {
+            .eof => debug.print(" at end", .{}),
+            .@"error" => {},
+            else => debug.print(" at '{s}'", .{token.lexeme}),
+        }
+        debug.print(": {s}\n", .{message});
+        self.had_error = true;
+    }
+
+    fn errorAtCurrent(self: *Self, message: []const u8) void {
+        self.errorAt(self.current, message);
+    }
+
+    pub fn consume(self: *Self, ty: Token.Type, message: []const u8) void {
+        if (self.current.ty != ty) {
+            self.errorAtCurrent(message);
+        } else {
+            self.advance();
+        }
+    }
+
+    pub fn match(self: *Self, ty: Token.Type) bool {
+        if (!self.check(ty)) return false;
+        self.advance();
+        return true;
+    }
+
+    fn check(self: Self, ty: Token.Type) bool {
+        return self.current.ty == ty;
+    }
+};
